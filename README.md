d4b5b02a7aa15236f86f4043cd20b0699ac9a8af
Aadam Zocolo <azocolo@gmail.com>
2020-11-25 00:22:05 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# py-controller-model
+
+Simplifies arbitrary API creation by leaving definition and binding in-code. 
+
+Goals:
+- [ ] Bind a controller to a model
+- [ ] Register using decorators 
+- [ ] Easy documentation (and basic introspection?)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='controllermodel',
+    version='0.1',
+    description='A simple controller-model implementation.',
+    url='',
+    author='Aadam Zocolo',
+    author_email='azocolo@gmail.com',
+    license='MIT',
+    packages=['controllermodel'],
+    zip_safe=False)

d3ec287a72863e47a2ac273997c3e42d609bc11a
Aadam Zocolo <azocolo@gmail.com>
2020-12-04 02:38:15 +0000

    GenericController functionality

--- controllermodel/controller/controller.py
+++ controllermodel/controller/controller.py
@@ -1,3 +1,7 @@
+"""
+The file contains the code for the GenericController class.
+"""
+
 import inspect
 import functools
 from typing import Dict, T, Callable
@@ -6,37 +10,52 @@ from .controllerinterface import ControllerInterface
 
 class GenericController(ControllerInterface):
     """
-    Creates a controller for a model. 
+    The base type Controller for a model. This implementation is barebones and very generic, but still perfectly usable. 
     """
 
-    def __init__(self, model_instance):
-        self._class_types = {} #A dictionary of `class: dict[action, function]`
+    _registered_classes = {}   # Dict of {
+                                #           klass: {
+                                #               action: method        
+                                #           }
+                                #       }
+
+    def __init__(self, instance_of_class: T):
+        print(GenericController._registered_classes)
+        if not instance_of_class.__class__.__qualname__ in GenericController._registered_classes:
+            raise Exception(f"Class {instance_of_class} has not been regestered with this controller class (yet?)!")
+
+        self._cls_instance = instance_of_class
+        self._func_set = self._registered_classes[instance_of_class.__class__.__qualname__]
 
     @classmethod
     def register_model(cls, _reg_cls: T):
-        #TODO: Register class type for correlation later!
+        """
+        This is a decorator funciton.
+
+        Does nothing right now.
+        Probably won't do anything for awhile. I see no use.
+        """
 
         # This doesn't get called until all of the functions are done being defined
         # in the potential model class. 
 
-        print(_reg_cls)
+        #cls._registered_classes[str(_reg_cls.__qualname__)] don't do nothin
 
         # The biggest challenge is linking the instance to its type's registered actions. 
-        # We will have to register one model (and treat subsequent registers as overwrites)
-        # to correlate to the registered actions. 
-        #!Is there a better way??
+        # When this function is used as a decorator 
+        return _reg_cls
 
-        pass
-
-    def register_action(self, _func=None, *args, action=None, description=None, **kw):
+    @classmethod
+    def register_action(cls, _func=None, *args, action=None, description=None, **kw):
         """
-        Eventually, will register a function as an action to do stuff on this controller.
+        This is a decorator function.
+
+        Will register a function as an action to do stuff on this controller.
 
         Tries to grab the action name from the function name, and a description from the functions 
         docstring.
         """
 
-        #TODO: Get the instance the function was registered from if possible! We can't call unbound functions! 
         #If we can figure out how to grab class instances, we can hotwire them into the function calls.
 
         def wrapper_func(func, *a, action=None, description=None):
@@ -54,11 +73,29 @@ class GenericController(ControllerInterface):
             else:
                 description = inspect.getdoc(func)
 
-            # self._actions[action] = (class_type, func)
-            # self._descriptions[action] = description
+            owning_class = None
+            try:
+                qn = str(func.__qualname__)
+                qn = qn.replace(func.__name__, '')
+                if qn[-1] == '.':
+                    qn = qn[:-1]
+                owning_class = qn
+            except AttributeError:
+                raise Exception("Function does not have a valid qualifying name! Is this function a member of a class?")
+            if owning_class == '':
+                raise Exception("Function does not have a valid qualifying name! Is this function a member of a class?")
+            print(f"Owning class is {owning_class}")
+            cls._registered_classes[owning_class] = {action: func}
+
             return func
 
         if _func == None:
             return wrapper_func
         else:
             return wrapper_func(_func, action=action, description=description)
+
+    def execute_action(self, action: str, *a, **kw):
+        if action in self._func_set:
+            return self._func_set[action](self._cls_instance, *a, **kw)
+        else:
+            return None
